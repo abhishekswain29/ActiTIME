4580d18fcabee5e9120c21036f3434e66734a0f4
Ricky <Ricky@192.168.2.16>
2017-03-05 19:59:33 +0530

    excel setup

--- src/script/VerifyInvalidLogin.java
+++ src/script/VerifyInvalidLogin.java
@@ -4,17 +4,24 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import generic.BaseTest;
+import generic.Excel;
 import pom.LoginPage;
 
 public class VerifyInvalidLogin extends BaseTest {
 	@Test
-	public void testVerifyInvalidLogin(){
+	public void testVerifyInvalidLogin() throws InterruptedException{
 		SoftAssert s = new SoftAssert();
 		LoginPage l = new LoginPage(driver);
-		l.setUserName("abc");
-		l.setPassword("xyz");
+		int rc = Excel.getRowCount(INPUT_PATH, "VerifyInvalidLogin");
+		for (int i = 1; i <=rc; i++) {
+		String un = Excel.getCellValue(INPUT_PATH, "VerifyInvalidLogin", i, 0);
+		String pw = Excel.getCellValue(INPUT_PATH, "VerifyInvalidLogin", i, 1);
+		l.setUserName(un);
+		l.setPassword(pw);
 		l.clickLogin();
 		l.verifyErrIsPresent(s);
+		Thread.sleep(2000);
+		}
 		s.assertAll();
 	}
 	
--- src/script/VerifyLoginLogout.java
+++ src/script/VerifyLoginLogout.java
@@ -3,22 +3,28 @@ package script;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
+import generic.Excel;
 import pom.EnterTimeTrackPage;
 import pom.LoginPage;
 
 public class VerifyLoginLogout extends BaseTest {
 @Test
-public void testVerifyLoginLogout(){
+public void testVerifyLoginLogout() throws InterruptedException{
 	LoginPage l = new LoginPage(driver);
 	
 	//enter valid un
-	l.setUserName("admin");
+	String un = Excel.getCellValue(INPUT_PATH, "VerifyLoginLogout", 1, 0);
+	String pw = Excel.getCellValue(INPUT_PATH, "VerifyLoginLogout", 1, 1);
+	l.setUserName(un);
 	//enter valid pw
-	l.setPassword("manager");
+	l.setPassword(pw);
 	//click login
 	l.clickLogin();
 	//verify Home Page
 	EnterTimeTrackPage e = new EnterTimeTrackPage(driver);
-	e.verifyTitle("actiTIME - Enter Time-Track");
+	String eTitle = Excel.getCellValue(INPUT_PATH, "VerifyLoginLogout", 1, 2);
+	e.verifyTitle(eTitle);
+	e.clickLogout();
+	Thread.sleep(2000);
 }
 }
